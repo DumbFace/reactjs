5d2c9710691488ca3e3a81f086962f4cfbbf29c1
DESKTOP-B9EFHAU\Khang <khang.pham.phuong@gosell.vn>
2024-03-31 01:53:32 +0700

    Add debounce

---
+++ core/hook/use-debouce/src/App.tsx
@@ -0,0 +1,110 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// eslint-disable-next-line no-undef
+
+import { FilterOutlined } from "@ant-design/icons";
+import { Badge, Button, Flex, Popover } from "antd";
+import React, { useState, memo } from "react";
+import "./App.css";
+import { Filter } from "./components/filter";
+import { ISelectProps } from "./components/filter";
+import { StatusStudent } from "./components/filter";
+const initStudents: Student[] = [
+  {
+    name: "Nguyen An",
+    age: 12,
+    grade: "3",
+    status: StatusStudent.IsScholarship,
+  },
+  {
+    name: "Tran Binh",
+    age: 16,
+    grade: "4",
+    status: StatusStudent.IsScholarship,
+  },
+  {
+    name: "Le Chi",
+    age: 10,
+    grade: "2",
+    status: StatusStudent.IsPoor,
+  },
+  {
+    name: "Pham Dung",
+    age: 14,
+    grade: "3",
+    status: StatusStudent.IsPoor,
+  },
+  {
+    name: "Vu Hang",
+    age: 11,
+    grade: "2",
+    status: StatusStudent.IsPoor,
+  },
+];
+
+interface Student {
+  name: string;
+  age: number;
+  grade: string;
+  status: StatusStudent;
+}
+
+interface IListStudentsProps {
+  students: Array<Student>;
+}
+
+const ListStudents = memo((props: IListStudentsProps) => {
+  const { students } = props;
+  console.log("Redner list student: ");
+  return (
+    <ul>
+      {students.map((student, index) => {
+        return <li key={index}>{student.name}</li>;
+      })}
+    </ul>
+  );
+});
+
+function App() {
+  const [students, setStudents] = useState<Array<Student>>(initStudents);
+  const [countFilter, setCountFilter] = useState<number>(0);
+  const [open, setOpen] = useState<boolean>(false);
+  const handleOpenChange = (open: boolean) => {
+    setOpen(open);
+  };
+
+  const handleFilterStudent = (selected: ISelectProps) => {
+    if (!selected) return;
+    let newStudent: Array<Student> = initStudents;
+
+    if (selected.status.length > 0) {
+      newStudent = initStudents.filter((student) =>
+        selected.status.includes(student.status)
+      );
+    }
+    setCountFilter(selected.status.length);
+    setStudents([...newStudent]);
+  };
+
+  return (
+    <React.Fragment>
+      <div style={{ display: "flex", gap: "126px" }}>
+        <Popover
+          content={<Filter filterStudent={handleFilterStudent}></Filter>}
+          title="Title"
+          trigger="click"
+          open={open}
+          onOpenChange={handleOpenChange}
+        >
+          <Badge count={countFilter}>
+            <Button icon={<FilterOutlined />} type="primary"></Button>
+          </Badge>
+        </Popover>
+
+        <ListStudents students={students}></ListStudents>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ core/hook/use-debouce/src/components/filter.tsx
@@ -0,0 +1,96 @@
+/* eslint-disable no-case-declarations */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Button, Space } from "antd";
+import React, { useEffect, useMemo, useState } from "react";
+import "./filter.style.scss";
+import { useDebounce } from "use-debounce";
+
+export enum StatusStudent {
+  IsPoor = 1,
+  IsScholarship = 2,
+}
+
+export interface IStudentStatus {
+  isPoor: boolean;
+  isScholarship: boolean;
+}
+
+export interface ISelectProps {
+  status: Array<number>;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const Filter = (props: any) => {
+  const { filterStudent } = props;
+
+  const [isPoorSelect, setIsPoor] = useState(false);
+  const [isScholarShipSelect, setScholarShip] = useState(false);
+  const [status, setStatus] = useState<Array<number>>([]);
+  const [selected, setSelected] = useState<ISelectProps>();
+  const [debouce] = useDebounce(selected,1000);
+
+  useEffect(() => {
+    filterStudent(selected);
+    console.log("selected: ", selected);
+  }, [debouce]);
+
+  const onHandleClickButton = (typeButton: number) => {
+    let newStatus: Array<number> = [];
+    switch (typeButton) {
+      case StatusStudent.IsPoor:
+        setIsPoor(!isPoorSelect);
+        if (!isPoorSelect) {
+          newStatus = [...status, StatusStudent.IsPoor];
+        } else {
+          newStatus = [...status].filter(
+            (status) => status !== StatusStudent.IsPoor
+          );
+        }
+        setStatus(newStatus);
+        setSelected({
+          ...selected,
+          status: newStatus,
+        });
+        break;
+      case StatusStudent.IsScholarship:
+        setScholarShip(!isScholarShipSelect);
+        if (!isScholarShipSelect) {
+          newStatus = [...status, StatusStudent.IsScholarship];
+        } else {
+          newStatus = [...status].filter(
+            (status) => status !== StatusStudent.IsScholarship
+          );
+        }
+        setStatus(newStatus);
+        setSelected({
+          ...selected,
+          status: newStatus,
+        });
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <Space className="filter">
+        <Button
+          onClick={() => onHandleClickButton(StatusStudent.IsPoor)}
+          type={isPoorSelect ? "primary" : undefined}
+          value={StatusStudent.IsPoor}
+        >
+          Age
+        </Button>
+        <Button
+          onClick={() => onHandleClickButton(StatusStudent.IsScholarship)}
+          type={isScholarShipSelect ? "primary" : undefined}
+          value={StatusStudent.IsScholarship}
+        >
+          Grade
+        </Button>
+      </Space>
+    </React.Fragment>
+  );
+};
